d0db6167a8e1a03ff53ead7641403000ea11b9e1
luohao <luohao@authine.com>
2021-07-11 23:24:09 +0800

    更改命名

---
+++ src/components/month-range-picker/header.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="custom-header">
+    <template v-if="option === 'prev'">
+      <a-icon type="left" class="left" @click="onPrevClick" />
+      <label>{{ value | formaMonth }}</label>
+      <span></span>
+    </template>
+    <template v-else>
+      <span></span>
+      <label>{{ value | formaMonth }}</label>
+      <a-icon type="right" class="right" @click="onNextClick" />
+    </template>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+import { Icon } from "ant-design-vue";
+
+export default {
+  name: "CustomHeader",
+  components: {
+    AIcon: Icon,
+  },
+  props: {
+    value: {
+      required: false,
+    },
+    option: {
+      required: true,
+    },
+    onChange: {
+      type: Function,
+      default: () => {},
+    },
+  },
+  filters: {
+    formaMonth(value) {
+      if (value) {
+        return moment(value).format("YYYY-MM");
+      } else {
+        return value;
+      }
+    },
+  },
+  methods: {
+    onPrevClick() {
+      if (this.value) {
+        this.onChange(moment(this.value).subtract(1, "years"));
+      }
+    },
+    onNextClick() {
+      if (this.value) {
+        this.onChange(moment(this.value).add(1, "years"));
+      }
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+.custom-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 16px 12px 16px;
+  .left,
+  .right {
+    cursor: pointer;
+  }
+}
+</style>
